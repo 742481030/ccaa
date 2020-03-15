6c1270b511891c2f149db93c4cda1354b4fdec30
742481030 <59884249+742481030@users.noreply.github.com>
2020-03-15 17:25:01 +0800

    Update ccaa.sh

--- ccaa.sh
+++ ccaa.sh
@@ -13,7 +13,7 @@ if [ $1 = 'cdn' ]
 	aria2_url='https://pan.mxin.ltd/?/shell/aria2c/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
 	filebrowser_url='https://pan.mxin.ltd/?/shell/aria2c/linux-amd64-filebrowser.tar.gz'
 	master_url='https://github.com/helloxz/ccaa/archive/master.zip'
-	ccaa_web_url='http://soft.xiaoz.top/linux/ccaa_web.tar.gz'
+	ccaa_web_url='https://pan.mxin.ltd/?/shell/aria2c/ccaa_web.tar.gz'
 	else
 	aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
 	filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.0.16/linux-amd64-filebrowser.tar.gz'
